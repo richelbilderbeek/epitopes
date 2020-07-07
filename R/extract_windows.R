8e88c4973054c2a2cf00ad7ee5ca880ebe62fa85
Felipe Campelo <fcampelo@gmail.com>
2020-07-07 17:24:29 +0100

    Update extract_windows.R

--- R/extract_windows.R
+++ R/extract_windows.R
@@ -5,6 +5,7 @@ extract_windows <- function(x, window_size, step_size, window_exp){
   # Initialise dataframe
   wdf <- data.frame(window_seq = rep(NA_character_, x$epitope_len + 2),
                     window_exp = rep(NA_character_, x$epitope_len + 2),
+                    center_pos = rep(NA_integer_, x$epitope_len + 2),
                     Class      = x$Class,
                     epitope_id = x$epitope_id,
                     protein_id = x$protein_id,
@@ -26,6 +27,7 @@ extract_windows <- function(x, window_size, step_size, window_exp){
   stop <- FALSE
   while(!stop){
     wdf$window_seq[j] <- substr(x$protein_seq, i1, i2)
+    wdf$center_pos[j] <- x$epitope_start + (j - 1) * step_size
     if(window_exp > 0) wdf$window_exp[j] <- substr(x$protein_seq, i1e, i2e)
     i1  <- i1 + step_size
     i2  <- i2 + step_size
