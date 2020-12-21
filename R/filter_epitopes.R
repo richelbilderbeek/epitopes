09997cb9102fc2b50a4404205ea08386cd60fa9e
Felipe Campelo <fcampelo@gmail.com>
2020-12-21 19:28:18 +0000

    minor update in filter_epitopes

--- R/filter_epitopes.R
+++ R/filter_epitopes.R
@@ -90,8 +90,10 @@ filter_epitopes <- function(df,
 
   if (!is.null(removeIDs)){
     idx3 <- which(sapply(ids$org, FUN = fmatch, trg = target_rm))
-    ids  <- ids[-idx3, ]
-    df <- df[-idx3, ]
+    if (length(idx3) > 0){
+      ids  <- ids[-idx3, ]
+      df <- df[-idx3, ]
+    }
   }
 
 
