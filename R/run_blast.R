94504c05602580c4a32da0361e9ff4fb82fcd934
fcampelo <fcampelo@gmail.com>
2021-08-12 17:54:40 +0100

    Update run_blast to simplify call (and make it compatible with v. 2.12.0)

--- R/run_blast.R
+++ R/run_blast.R
@@ -11,21 +11,15 @@ run_blast <- function(BLAST_path, prots, ncpus){
   system(paste0("makeblastdb -in ", fn, " -dbtype prot"))
 
   message("===========================================\nRunning BLASTp\n")
-  system(paste0("blastp -num_threads ", ncpus, " -query ", fn, " -db ", fn, " -seg no ",
-                "-outfmt '6 qseqid sseqid length qlen slen nident pident",
-                " qcovhsp mismatch gaps qstart qend sstart send evalue score' > ",
+  system(paste0("blastp -query ", fn, " -db ", fn, " -seg no ",
+                "-outfmt '6 qseqid sseqid pident qcovhsp' > ",
                 fn, "-BLAST"))
 
   blast <- utils::read.csv(paste0(fn, "-BLAST"), sep = "\t",
                            header = FALSE,
                            stringsAsFactors = FALSE)
 
-  names(blast) <- c("QueryID", "SubjectID", "Alignment_length",
-                    "Query_length", "Subject_length", "Num_matches",
-                    "Perc_identity", "Query_coverage", "Num_mismatches",
-                    "Num_gaps", "Query_match_start", "Query_match_end",
-                    "Subject_match_start", "Subject_match_end",
-                    "E_value", "Score")
+  names(blast) <- c("QueryID", "SubjectID", "Perc_identity", "Query_coverage")
 
   blast$QueryID   <- gsub("\\.[1-9]+$", "", blast$QueryID)
   blast$SubjectID <- gsub("\\.[1-9]+$", "", blast$SubjectID)
